4a4bd43d453b6d305617a2dfa02528c9a65c2843
Willem Wannenburg <daeben72@gmail.com>
2024-05-26 23:57:50 +0200

    Made an interface for tracks and recently played

---
+++ interfaces/tracks.ts
@@ -0,0 +1,83 @@
+export interface RecentlyPlayed {
+  href: string;
+  limit: number;
+  next: string | null;
+  cursors: {
+    after: string;
+    before: string;
+  };
+  total?: number;
+  items: PlayHistoryObject[];
+}
+
+export interface ExternalURLs {
+  spotify: string;
+}
+
+export interface Context {
+  type: string;
+  href: string;
+  external_urls: {
+    spotify: string;
+  };
+  uri: string;
+}
+
+export interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
+
+export interface Album {
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalURLs;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  type: string;
+  uri: string;
+  artists: Artist[];
+}
+
+export interface Artist {
+  external_urls: ExternalURLs;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
+export interface Track {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: {
+    isrc: string;
+  };
+  external_urls: ExternalURLs;
+  href: string;
+  id: string;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+export interface PlayHistoryObject {
+  played_at: string;
+  context: Context | null;
+  track: Track;
+}
