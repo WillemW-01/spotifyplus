f3a4f91980460fb94329e9345e653a0096560afe
Willem Wannenburg <daeben72@gmail.com>
2024-05-27 18:24:46 +0200

    Fixed the gradient background issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,27 @@ import { AuthProvider } from "@/hooks/AuthContext";
 import { GlobalProvider } from "@/hooks/Globals";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme, SafeAreaView } from "react-native";
 
 export default function RootLayout() {
   const theme = useColorScheme() ?? "dark";
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: Colors[theme]["background"] }}
-    >
-      <StatusBar style="dark" />
-      <AuthProvider>
-        <GlobalProvider>
+    <AuthProvider>
+      <GlobalProvider>
+        <StatusBar style="dark" />
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: Colors[theme]["background"] }}
+        >
           <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" />
             <Stack.Screen name="home" />
           </Stack>
-        </GlobalProvider>
-      </AuthProvider>
-    </SafeAreaView>
+        </SafeAreaView>
+        <SafeAreaView
+          style={{ flex: 0, backgroundColor: Colors[theme]["brand"] }}
+        />
+      </GlobalProvider>
+    </AuthProvider>
   );
 }
