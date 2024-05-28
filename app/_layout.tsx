703dacac62b83001f5db10a223b1afdff0f6d7f2
Willem Wannenburg <daeben72@gmail.com>
2024-05-28 13:50:30 +0200

    Added custom fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,30 @@
 import { Colors } from "@/constants/Colors";
 import { AuthProvider } from "@/hooks/AuthContext";
 import { GlobalProvider } from "@/hooks/Globals";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import { StatusBar } from "react-native";
 import { useColorScheme } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const theme = useColorScheme() ?? "dark";
+
+  const [loaded] = useFonts({
+    Inter: require("../assets/fonts/Inter-Regular.ttf"),
+    InterBold: require("../assets/fonts/Inter-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <AuthProvider>
