b83ea093009db076dd408606871482079387214b
Willem Wannenburg <daeben72@gmail.com>
2024-08-01 18:31:04 +0200

    Changed to pnpm to test it out

--- app/(tabs)/mood.tsx
+++ app/(tabs)/mood.tsx
@@ -1,13 +1,66 @@
-import React from "react";
-
+import { PlayHistoryObject } from "@/interfaces/tracks";
+import { useAuth } from "@/hooks/AuthContext";
+import { usePlayback } from "@/hooks/usePlayback";
+import { useRequestBuilder } from "@/hooks/useRequestBuilder";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, useColorScheme } from "react-native";
+import { Colors } from "@/constants/Colors";
+import Card from "@/components/Card";
 import BrandGradient from "@/components/BrandGradient";
 import ThemedText from "@/components/ThemedText";
-import LoaderCard from "@/components/LoaderCard";
 
 export default function Mood() {
+  const [recents, setRecents] = useState<PlayHistoryObject[]>([]);
+
+  const { authorized } = useAuth();
+  const { getRecent, playTrack } = usePlayback();
+  const theme = useColorScheme() ?? "dark";
+
+  const fetchRecents = async () => {
+    const response = await getRecent();
+    setRecents(response);
+  };
+
+  useEffect(() => {
+    if (authorized) {
+      fetchRecents();
+    }
+  }, []);
+
   return (
-    <BrandGradient>
-      <ThemedText type="title" text="Mood" />
+    <BrandGradient style={{ flex: 1, alignItems: "center", gap: 30 }}>
+      <Text style={{ fontSize: 35, color: Colors[theme]["light"] }}>
+        Recently Played:
+      </Text>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: 30,
+          width: "100%",
+        }}
+        style={{
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        {recents &&
+          recents.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                title={item.track.name}
+                subtitle={item.track.artists[0].name}
+                imageUri={item.track.album.images[0].url}
+                onPress={() => {
+                  playTrack(item.track.id);
+                }}
+                width={90}
+              />
+            );
+          })}
+      </ScrollView>
     </BrandGradient>
   );
 }
